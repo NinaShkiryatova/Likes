d387d195c91198062604a589851dd88b0c6b852f
Нина Шкирятова <WildForests@yahoo.com>
2022-03-07 11:58:36 +0900

    Скорректирован файл main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,5 @@
-val likesCount: Int = 1
-val persons: String = if ((likesCount % 10) == 1 && (likesCount % 100) !== 11) "человеку" else "людям"
-
 fun main() {
+    val likesCount: Int = 111
+    val persons: String = if ((likesCount % 10) == 1 && (likesCount % 100) !== 11) "человеку" else "людям"
     println("Понравилось $likesCount $persons.")
 }
